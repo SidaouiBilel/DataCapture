cb5ce0808d802709484a2468940115373ab54ffc
HassenMahdi <hassenmahdi77@gmail.com>
2020-07-30 12:02:28 +0200

    View Target Component

---
+++ src/app/datacapture/pages/upload/components/preview/view-modes/target-preview/target-preview.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-target-preview',
+  templateUrl: './target-preview.component.html',
+  styleUrls: ['./target-preview.component.css']
+})
+export class TargetPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  filename$
+  activePipe$
+  
+  ngOnInit() {
+  }
+
+}
