353ab1cd2210f75916b3597b7fbefa3a879bdc48
HassenMahdi <hassenmahdi77@gmail.com>
2020-08-25 16:45:18 +0200

    better UI

--- src/app/datacapture/pages/upload/components/transformation/transformations/transformation-interface/format/filter/filter.component.ts
+++ src/app/datacapture/pages/upload/components/transformation/transformations/transformation-interface/format/filter/filter.component.ts
@@ -8,7 +8,17 @@ import { TransformationInterfaceComponent } from '../../transformation-interface
 })
 export class FilterComponent extends TransformationInterfaceComponent implements OnInit  {
 
-  operations = ['>','<', '>=','<=','==','!=']
+  operations = [
+    {value: '>', label: 'Greater'},
+    {value: '<', label: 'Lesser'}, 
+    {value: '>=', label: 'Greater or Equal'},
+    {value: '<=', label: 'Lesser or Equal'},
+    {value: '==', label: 'Equal'},
+    {value: '!=', label: 'Not Equal'}, 
+    {value: 'contains', label: 'Contains'}, 
+    {value: 'startswith', label: 'Starts With'}, 
+    {value: 'endswith', label: 'Ends With'},
+  ]
   
   constructor() { 
     super()
