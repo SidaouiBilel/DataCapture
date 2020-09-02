28f774a031936edf17315f6ff47d82528760952d
HassenMahdi <hassenmahdi77@gmail.com>
2020-09-02 10:45:29 +0200

    domains fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   // upload: 'http://localhost:5000/import/',
   mapping: 'https://dcm-mapping.azurewebsites.net/mapping',
   // mapping: 'http://localhost:5001/mapping',
-  cleansing: 'https://dcm-check.azurewebsites.net/check',
+  cleansing: 'https://dcm-datacheck.azurewebsites.net/check',
   // TRANSFORMATION
   // transform: 'http://localhost:5000/transfo/'
   transform: 'https://dcm-transforming.azurewebsites.net/transfo/',
