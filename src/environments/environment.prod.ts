4295fc9da202e3a8d1b7cb1a0da4e505577546da
Wajdovic <wajdibenabderrahim@gmail.com>
2020-09-22 17:15:18 +0200

    deepKube Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,16 +6,16 @@ export const environment = {
   ...meta,
   production: true,
   test: false,
-  admin: 'https://dcm-admin.azurewebsites.net/',
-  import: 'https://dcm-import.azurewebsites.net/import/',
+  admin: 'http://localhost:5002/',
+  import: 'http://localhost:5000/import/',
   // upload: 'http://localhost:5000/import/',
-  mapping: 'https://dcm-mapping.azurewebsites.net/mapping',
+  mapping: 'http://localhost:5003/mapping',
   // mapping: 'http://localhost:5001/mapping',
-  cleansing: 'https://dcm-datacheck.azurewebsites.net/check',
+  cleansing: 'http://localhost:5005/check',
   // TRANSFORMATION
   // transform: 'http://localhost:5000/transfo/'
-  transform: 'https://dcm-transforming.azurewebsites.net/transfo/',
-  upload: 'https://dcm-upload.azurewebsites.net/upload/',
-  auth: 'https://dcm-auth.azurewebsites.net/',
+  transform: 'http://localhost:5004/transfo/',
+  upload: 'http://localhost:5006/upload/',
+  auth: 'http://localhost:5001/',
   env: 'PRD'
 };
