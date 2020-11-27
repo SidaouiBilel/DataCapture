7e1172478e66eeab9639e33150b52421752ce8a6
hamzajorfaoui <hamzajorfaoui@gmail.com>
2020-11-27 12:56:19 +0100

    fix

--- src/app/datacapture/pages/template/table-template/table-template.component.ts
+++ src/app/datacapture/pages/template/table-template/table-template.component.ts
@@ -22,11 +22,37 @@ export class TableTemplateComponent implements OnInit {
 
   ngOnInit() {
      this.setlistOfcolumns(this.templatedata);
-    console.log(Object.keys(this.templatedata).map(el=>({
-      title:el,
-      ...{"dataArray":Array.isArray(this.templatedata[el]) ?this.templatedata[el] :null,
-       "dataobj":!Array.isArray(this.templatedata[el]) ?this.templatedata[el] :null},
-    })))
+
+
+     //testing
+     
+    //  let list = [];
+    //  let values = [...Object.values(this.templatedata)];
+    //  let keys = [...Object.keys(this.templatedata)];
+    //  values.map((el)=>{
+    //   if(!Array.isArray(el)  ){
+    //     if(list.indexOf(el["sheet"])==-1){
+    //        list.push(el["sheet"]);
+    //     console.log(el["sheet"])
+    //     }
+    //   }else{
+    //     el.map(el=>{
+    //       if(list.indexOf(el["sheet"])==-1){
+    //         list.push(el["sheet"]);
+    //         console.log(el["sheet"])
+    //      }
+    //     })
+    //   }
+
+    //  })
+
+    // console.log(Object.keys(this.templatedata).map(el=>({
+    //   title:el,
+    //   ...{"dataArray":Array.isArray(this.templatedata[el]) ?this.templatedata[el] :null,
+    //       "dataobj":!Array.isArray(this.templatedata[el]) ?this.templatedata[el] :null},
+    // })))
+
+    // console.log(list)
   }
 
 }
