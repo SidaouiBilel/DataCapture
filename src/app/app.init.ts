b68eafe67472a966eecef7a3a7e480a3a8677535
hamzajorfaoui <hamzajorfaoui@gmail.com>
2021-03-03 23:04:00 +0100

    fix get data-config

--- src/app/app.init.ts
+++ src/app/app.init.ts
@@ -1,15 +1,14 @@
 import { environment } from '@env/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import {updateConfig} from "./env.service";
 @Injectable()
 export class AppInitService {
 
-    constructor(private http: HttpClient){}
+    constructor(){}
 
     public init() {
         return new Promise((resolve, reject) => {
-            fetch('assets/data-config.json?nocache='+(new Date()).getTime())
+            fetch(environment.deployURL+'assets/data-config.json?nocache='+(new Date()).getTime())
                 .then(res=>(res.json()))
                 .then(config => {
                     // console.log({...environment , ...config});
