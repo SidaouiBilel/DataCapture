4feb42c572a96d965b86409318f80261c36ce494
KarimBourass <bourasskarim9@gmail.com>
2021-04-26 15:57:52 +0000

    Add transformation node component

---
+++ src/app/datacapture/pages/manual-upload/componenets/transformations/components/transformation-node/transformation-node.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transformation-node',
+  templateUrl: './transformation-node.component.html',
+  styleUrls: ['./transformation-node.component.css']
+})
+export class TransformationNodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
