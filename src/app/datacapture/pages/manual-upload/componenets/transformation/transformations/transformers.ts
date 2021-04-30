b5666168563de6eaa9b62828fd62fd4ee940f1a0
BHM-Yassine <yassine.bouhm@edu.uiz.ac.ma>
2021-04-30 18:25:09 +0000

    Add Merge transformation

--- src/app/datacapture/pages/manual-upload/componenets/transformation/transformations/transformers.ts
+++ src/app/datacapture/pages/manual-upload/componenets/transformation/transformations/transformers.ts
@@ -1,11 +1,14 @@
 import { DeleteRow, Replace, Merge, Filter, FilterAndReplace, DefaultValue, Splitter, Calculator,
   FormatDate, GroupBy, Hasher, Joiner } from "@app/datacapture/pages/upload/components/transformation/transformations/transformers/transformer.model";
 import { FilterComponent } from "./trasnformation-intefrace/nodes/filter/filter.component";
+import { MergerComponent } from "./trasnformation-intefrace/nodes/merger/merger.component";
 import { NewCalculatorComponent } from "./trasnformation-intefrace/nodes/new-calculator/new-calculator.component";
 
 export const TRANSFORMATIONS = [
   new Calculator().setComponent(NewCalculatorComponent),
   new Filter().setComponent(FilterComponent),
+  new Merge().setComponent(MergerComponent),
+
 /*   ,
   new FilterAndReplace().setComponent(FindAndReplaceComponent),
   new Replace().setComponent(FormatterComponent),
