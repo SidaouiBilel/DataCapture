b41111225f5c488c4f73b9c145b3223cd3f5463a
KarinBourass <bourasskarim9@gmail.com>
2021-10-14 20:59:08 +0100

    adapt to new rsu API

--- src/app/datacapture/pages/admin/services/rsu.service.ts
+++ src/app/datacapture/pages/admin/services/rsu.service.ts
@@ -1,3 +1,4 @@
+import { log } from 'console';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@env/environment';
@@ -26,10 +27,14 @@ export class RsuService {
     return this.http.post(url, data);
   }
 
+  getFormResult2(data) {
+    return this.http.post('http://ec2-54-163-35-186.compute-1.amazonaws.com:80/predict_without_revenu', data);
+  }
+
   getUri(modele) {
     switch (modele) {
       case 0:
-        return 'ec2-54-163-35-186.compute-1.amazonaws.com:80/predict_with_revenu'
+        return 'ec2-54-163-35-186.compute-1.amazonaws.com:80/predict_without_revenu'
       case 1:
       case 2:
         return 'ec2-54-163-35-186.compute-1.amazonaws.com:80/predict_with_revenu'
